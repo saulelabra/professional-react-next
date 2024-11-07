5f6fe17c5b1a05f4ed9604fb400cb7e06dff9ad5
saulelabra <quiquelabra@gmail.com>
2024-11-07 13:40:35 -0600

    Project 2: Word Analytics

---
+++ word-analytics/src/components/Container.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import Stats from "./Stats";
+import Textarea from "./Textarea";
+import {
+  INSTAGRAM_MAX_CHARACTERS,
+  FACEBOOK_MAX_CHARACTERS,
+} from "../lib/constants";
+
+export default function Container() {
+  const [text, setText] = useState("");
+
+  const stats = {
+    numberOfWords: text.split(" ").filter((word) => word !== "").length,
+    numberOfCharacters: text.length,
+    instagramCharactersLeft: INSTAGRAM_MAX_CHARACTERS - text.length,
+    facebookCharactersLeft: FACEBOOK_MAX_CHARACTERS - text.length,
+  };
+
+  return (
+    <main className="container">
+      <Textarea text={text} setText={setText} />
+      <Stats stats={stats} />
+    </main>
+  );
+}
